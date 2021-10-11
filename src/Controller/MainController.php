14f96320fceb32f1cc5c0a2d810c1c446a68b704
anaiskaddioui <an.17@hotmail.fr>
2021-10-12 00:05:59 +0200

    Problème de configuration

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -10,9 +11,10 @@ class MainController extends AbstractController {
 
     /**
      * @Route("/", name="home")
+     * @return Response
      */
-    public function home()
+    public function home(): Response
     {
-        return $this->render('main/home.html.twig');
+        return $this->render('main/home/html/twig');
     }
 }
