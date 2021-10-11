5be78fc25494ad1435f32efb1cf7f8fdf9cfdd40
Charles <52407656+ChPu0@users.noreply.github.com>
2021-10-11 14:28:13 +0200

    Creation entités et relations

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -72,6 +72,11 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $campus;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $pseudo;
+
 
     public function __construct()
     {
@@ -262,6 +267,18 @@ class Participant implements UserInterface, PasswordAuthenticatedUserInterface
     public function setCampus(?Campus $campus): self
     {
         $this->campus = $campus;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
 
         return $this;
     }
